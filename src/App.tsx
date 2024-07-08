f7e6c81a4f8b52a4e721c399158a965856e55762
Yamin Faruque <yamin.faruque@sazim.io>
2024-07-08 19:09:20 +0600

    chore(FTDA):Fix formatting and remove unnecessary lines

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  
-
   return (
     <>
-<h1>APP</h1>
+      <h1>APP</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
